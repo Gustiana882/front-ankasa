dc31b40a20343fa3aa45e5db906bbb3a6e4a20da
calvinrahmat <calvin.rahmat@gmail.com>
2021-08-20 21:23:14 +0700

    Navbar Fix

--- src/Pages/Explore/Explore.jsx
+++ src/Pages/Explore/Explore.jsx
@@ -1,3 +1,4 @@
+import { Container } from 'react-bootstrap';
 import SearchFlight from '../../Components/Explore/SearchFlight';
 import TrendingCarousel from '../../Components/Explore/TrendingCarousel';
 import NavbarHeader from '../../Components/Navbar/NavbarHeader';
@@ -5,12 +6,26 @@ import './Explore.scoped.css';
 
 const Explore = () => {
 	return (
-		<div className="wrapper">
-			<NavbarHeader />
+		<div>
+			<div className="navbar-header">
+				<NavbarHeader />
+			</div>
+
 			<div className="SearchFlight">
 				<SearchFlight />
 			</div>
-			<TrendingCarousel />
+			<div className="trending-box">
+				<div className="title-trending">
+					<div className="left">
+						<p>TRENDING</p>
+						<p>Trending destinations</p>
+					</div>
+					<div className="right">
+						<p>View all</p>
+					</div>
+				</div>
+				{/* <TrendingCarousel /> */}
+			</div>
 		</div>
 	);
 };
