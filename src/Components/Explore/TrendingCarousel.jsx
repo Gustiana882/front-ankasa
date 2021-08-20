32820f9cb34c4e5a4a02a059ffda0764c65297a6
calvinrahmat <calvin.rahmat@gmail.com>
2021-08-20 16:19:19 +0700

    adding carousel

--- src/Components/Explore/TrendingCarousel.jsx
+++ src/Components/Explore/TrendingCarousel.jsx
@@ -3,36 +3,104 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { useState } from 'react';
+import { FaAngleRight } from 'react-icons/fa';
 
 export default function TrendingCarousel() {
 	var settings = {
 		dots: true,
 		infinite: true,
 		speed: 500,
-		slidesToShow: 1,
+		slidesToShow: 5,
 		slidesToScroll: 1,
 	};
+
+	const cities = [
+		{
+			city: 'Tokyo',
+			country: 'Japan',
+		},
+		{
+			city: 'Tokyo',
+			country: 'Japan',
+		},
+		{
+			city: 'Tokyo',
+			country: 'Japan',
+		},
+		{
+			city: 'Tokyo',
+			country: 'Japan',
+		},
+		{
+			city: 'Tokyo',
+			country: 'Japan',
+		},
+	];
+
 	const [destination, setDestination] = useState([]);
+
 	return (
-		<Slider {...settings}>
-			<div>
-				<h3>1</h3>
+		<div>
+			<div className="carousel-box">
+				<Slider {...settings}>
+					<div>
+						<div className="card">
+							<div className="location">
+								<p>Tokyo</p>
+								<p>Japan</p>
+							</div>
+							<FaAngleRight />
+						</div>
+					</div>
+					<div>
+						<div className="card">
+							<div className="title">
+								<div className="location">
+									<p>Tokyo</p>
+									<p>Japan</p>
+								</div>
+								<FaAngleRight />
+							</div>
+						</div>
+					</div>
+					<div>
+						<div className="card">
+							<div className="title">
+								<p>Tokyo</p>
+								<p>Japan</p>
+								<FaAngleRight />
+							</div>
+						</div>
+					</div>
+					<div>
+						<div className="card">
+							<div className="title">
+								<p>Tokyo</p>
+								<p>Japan</p>
+								<FaAngleRight />
+							</div>
+						</div>
+					</div>
+					<div>
+						<div className="card">
+							<div className="title">
+								<p>Tokyo</p>
+								<p>Japan</p>
+								<FaAngleRight />
+							</div>
+						</div>
+					</div>
+					<div>
+						<div className="card">
+							<div className="title">
+								<p>Tokyo</p>
+								<p>Japan</p>
+								<FaAngleRight />
+							</div>
+						</div>
+					</div>
+				</Slider>
 			</div>
-			<div>
-				<h3>2</h3>
-			</div>
-			<div>
-				<h3>3</h3>
-			</div>
-			<div>
-				<h3>4</h3>
-			</div>
-			<div>
-				<h3>5</h3>
-			</div>
-			<div>
-				<h3>6</h3>
-			</div>
-		</Slider>
+		</div>
 	);
 }
