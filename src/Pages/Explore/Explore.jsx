77717780d84a975dca870f0d4dbd76ecceed6c92
calvinrahmat <calvin.rahmat@gmail.com>
2021-08-20 08:44:09 +0700

    init

---
+++ src/Pages/Explore/Explore.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavbarHeader from '../../Components/Navbar/NavbarHeader';
+import SearchFlight from '../../Components/Modals/SearchFlight';
+
+const Explore = () => {
+	return (
+		<div>
+			<NavbarHeader />
+		</div>
+	);
+};
+
+export default Explore;
---
+++ src/app.js
@@ -0,0 +1,16 @@
+import NavbarHeader from './Components/Navbar/NavbarHeader';
+import SearchFlight from './Components/Modals/SearchFlight';
+import './app.scoped.css';
+import SearchFlightSummary from './Components/Navbar/SearchFlightSummary';
+import Explore from './Pages/Explore/Explore';
+import '/Explore.scoped.<i class="fa fa-css3" aria-hidden="true"></i>';
+
+function App() {
+	return (
+		<div>
+			<Explore />
+		</div>
+	);
+}
+
+export default App;
