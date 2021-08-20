45abc99ebbe1175675740652207e8969dde55e30
calvinrahmat <calvin.rahmat@gmail.com>
2021-08-20 08:46:48 +0700

    fixing

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import SearchFlight from './Components/Modals/SearchFlight';
 import './app.scoped.css';
 import SearchFlightSummary from './Components/Navbar/SearchFlightSummary';
 import Explore from './Pages/Explore/Explore';
-import '/Explore.scoped.<i class="fa fa-css3" aria-hidden="true"></i>';
 
 function App() {
 	return (
