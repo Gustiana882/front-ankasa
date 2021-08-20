ba3d0da6ef2287fc930495db430c30aba0b5d87f
firyal-salsa <firsabilacahyadi@gmail.com>
2021-08-20 21:54:56 +0700

    add a register file

--- src/Routers.js
+++ src/Routers.js
@@ -1,12 +1,10 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Login from "./Components/Login/login"
-import Register from "./Views/Register"
 
 function Routers() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/signup" component={Register} />
                 <Route exact path="/" component={Login} />
             </Switch>
         </BrowserRouter>
