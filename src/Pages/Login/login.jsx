fd4e87826a3d33ec9afaa97ab7065c78fc01b23f
calvinrahmat <calvin.rahmat@gmail.com>
2021-08-22 13:58:59 +0700

    connect to backend

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -1,107 +1,144 @@
-import React, { useState, useEffect } from "react";
-// import { useForm } from "react-hook-form"
-import "./login.scoped.css"
-import { Link } from "react-router-dom"
+import './login.scoped.css';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import axios from 'axios';
+import {
+	loginPending,
+	loginSuccess,
+	loginFail,
+	getEmail,
+	getToken,
+} from '../../Storages/Slices/loginSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { Spinner } from 'react-bootstrap';
+import { useHistory } from 'react-router';
+const url = `${process.env.REACT_APP_API}/login`;
+
+const schema = yup.object().shape({
+	email: yup
+		.string()
+		.email('please input valid email')
+		.required(),
+	password: yup
+		.string()
+		.min(5)
+		.required(),
+});
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+	const { isLoading } = useSelector((state) => state.loginReducer);
+	console.log(isLoading);
+	const dispatch = useDispatch();
+	const history = useHistory();
+	const { register, handleSubmit } = useForm({ resolver: yupResolver(schema) });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+	const onSubmit = (data) => {
+		try {
+			axios.post(url, data).then((res) => {
+				const { msg, token, email } = res.data.result[0];
+				if (msg === 'Login Success') {
+					sessionStorage.setItem('token', token);
+					dispatch(loginSuccess());
+					dispatch(getToken(token));
+					dispatch(getEmail(email));
+					history.push('/home');
+					console.log(msg);
+				} else {
+					dispatch(loginFail('invalid password or email'));
+				}
+			});
+		} catch (error) {
+			dispatch(loginFail(error.message));
+		}
+		dispatch(loginPending());
+	};
 
-    switch (name) {
-      case "username":
-        setUsername(value);
-        break;
-      case "password":
-        setPassword(value);
-        break;
-      default:
-        break;
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  // useEffect(() => {
-  //   const togglePassword = document.querySelector("#togglePassword");
-  //   const password = document.querySelector("#password");
-  //     // toggle the type attribute
-  //     const type =
-  //       password.getAttribute("type") === "password" ? "text" : "password";
-  //     password.setAttribute("type", type);
-  //     // toggle the eye / eye slash icon
-  //     this.classList.toggle("bi-eye");
-  // }, []);
-
-  return (
-    <div>
-      <div className="blueBanner">
-        <img
-          src="https://res.cloudinary.com/rizkazn/image/upload/v1629439835/ankasaBanner_kmjq8m.jpg"
-          alt=""
-        />
-      </div>
-            <aside>
-              <div className="formBanner">
-                <div className="formTitle">
-                  <img src="https://res.cloudinary.com/rizkazn/image/upload/v1629439821/logo_lgffvi.jpg" alt=""/>
-                  <span className="text-logo">
-                    <b>  Ankasa</b>
-                  </span>
-                    <div className="login-box">
-                      <h4><b>Login</b></h4>
-                      <form className="formRegis">
-                        <div className="user-box">
-                          <input 
-                          type="username" 
-                          name="username" 
-                          value={username} 
-                          onChange={handleChange} 
-                          required />
-                          <label>Username</label>
-                        </div>
-                        <div className="user-box">
-                          <input 
-                          type="password" 
-                          name="password" 
-                          value={password} 
-                          onChange={handleChange}
-                          required />
-                          <label>Password</label>
-                        </div>
-                        <button className="SignIn" 
-                        type="submit" 
-                        handleOnSubmit={handleSubmit}
-                        block>
-                          Sign In</button>
-                      </form>
-                      <br />
-                      <p>Did you forgot your password?</p>
-                      <Link to="/forgotpassword">Tap here for reset</Link>
-                      <br />
-                      <hr />
-                      <p>or sign in with</p>
-                      <div className="row mb-2">
-                        <div className="col-6">
-                          <button className="btn btn-block btn-social btn-google">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt=""/>
-                          </button>
-                        </div>
-                        <div className="col-6">
-                          <button className="btn btn-block btn-social btn-facebook">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/c/cd/Facebook_logo_%28square%29.png" alt=""/>
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-            </aside>
-          </div>
-)}
+	return (
+		<div>
+			<div className="blueBanner">
+				<img
+					src="https://res.cloudinary.com/rizkazn/image/upload/v1629439835/ankasaBanner_kmjq8m.jpg"
+					alt=""
+				/>
+			</div>
+			<aside>
+				<div className="formBanner">
+					<div className="formTitle">
+						<img
+							src="https://res.cloudinary.com/rizkazn/image/upload/v1629439821/logo_lgffvi.jpg"
+							alt=""
+						/>
+						<span className="text-logo">
+							<b> Ankasa</b>
+						</span>
+						<div className="login-box">
+							<h4>
+								<b>Login</b>
+							</h4>
+							<form className="formRegis" onSubmit={handleSubmit(onSubmit)}>
+								<div className="user-box">
+									<input
+										type="username"
+										name="username"
+										{...register('email')}
+										required
+									/>
+									<label>Username</label>
+								</div>
+								<div className="user-box">
+									<input
+										type="password"
+										name="password"
+										{...register('password')}
+										required
+									/>
+									<label>Password</label>
+								</div>
+								<button
+									className="SignIn"
+									type="submit"
+									handleOnSubmit={handleSubmit}
+									block
+								>
+									{isLoading ? (
+										<Spinner variant="primary" animation="border" />
+									) : (
+										<span>Sign In</span>
+									)}
+								</button>
+							</form>
+							<br />
+							<p>Did you forgot your password?</p>
+							<Link to="/forgotpassword">Tap here for reset</Link>
+							<br />
+							<hr />
+							<p>or sign in with</p>
+							<div className="row mb-2">
+								<div className="col-6">
+									<button className="btn btn-block btn-social btn-google">
+										<img
+											src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+											alt=""
+										/>
+									</button>
+								</div>
+								<div className="col-6">
+									<button className="btn btn-block btn-social btn-facebook">
+										<img
+											src="https://upload.wikimedia.org/wikipedia/commons/c/cd/Facebook_logo_%28square%29.png"
+											alt=""
+										/>
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</aside>
+		</div>
+	);
+};
 
 export default Login;
