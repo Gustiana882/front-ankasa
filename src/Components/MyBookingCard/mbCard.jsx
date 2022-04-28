729bd51f2d5babbbb75fecb223c24e7c153bc39a
Gusti13 <96718132+Gusti13@users.noreply.github.com>
2022-04-29 01:05:42 +0700

    perbaikan halaman eksplor untuk mobile

--- src/Components/MyBookingCard/mbCard.jsx
+++ src/Components/MyBookingCard/mbCard.jsx
@@ -1,66 +1,100 @@
-import React from "react"
-import { Card, Col, Container, Dropdown, Row} from "react-bootstrap"
-import "./mbCard.scoped.css"
+import React from 'react';
+import { Card, Col, Container, Dropdown, Row } from 'react-bootstrap';
+import './mbCard.scoped.css';
 
-const mbCard = () => {
-    return (
-        <div>
-        <Container fluid>
-            <Col>
-            <Card className="card">
-                <Card.Text className="text-up"><span>MY BOOKING </span></Card.Text>
-                    <Row>
-                        <Col sm={6}>
-                                <Card.Text className="text-row1"><span><b>My Booking </b></span></Card.Text>
-                        </Col>
-                         <Col sm={6}>
-                                <Card.Text className="text-row2"><span>Order History </span></Card.Text>
-                        </Col>
-                        </Row>
-                </Card>
-            </Col>
+const MbCard = (props) => {
+	return (
+		<div>
+			<Container fluid>
+				<Col>
+					<Card className="card">
+						<Card.Text className="text-up">
+							<span>MY BOOKING </span>
+						</Card.Text>
+						<Row>
+							<Col sm={6}>
+								<Card.Text className="text-row1">
+									<span>
+										<b>My Booking </b>
+									</span>
+								</Card.Text>
+							</Col>
+							<Col sm={6}>
+								<Card.Text className="text-row2">
+									<span>Order History </span>
+								</Card.Text>
+							</Col>
+						</Row>
+					</Card>
+				</Col>
 
-            <Col>
-            <Card className="card">
-                <Card.Text><span>Monday, 20 July '20' - 12:33</span></Card.Text>
-                <Card.Text className="text-row1"><span><b>FROM   
-                    <img src="https://res.cloudinary.com/rizkazn/image/upload/v1629481935/Vector_opjk1b.jpg" 
-                        style={{
-                            paddingLeft: '20px',
-                            paddingRight: '20px',
-                            }}
-                            alt="">
-                    </img> 
-                    TO</b></span></Card.Text>
-                <Card.Text className=""><span>MASKAPAI, KODE MASKAPAI </span></Card.Text>
-                    <hr />
-                    <br />
-                    <Row>
-                        <Col sm={3}>
-                            <Card.Text><span><b>Status</b></span></Card.Text>
-                        </Col>
-                        <Col sm={5}>
-                            <span className="status">Waiting for payment</span>
-                        </Col>
-                         <Col sm={4}>
-                         <Dropdown >
-                            <Dropdown.Toggle 
-                            style={{
-                                float: "right",
-                                borderStyle: 'none',
-                                outline: 'none',
-                                background: 'none',
-                                color: '#2395FF'
-                            }}>View Details</Dropdown.Toggle>
-                         </Dropdown>
-                        </Col>
-                    </Row>
-                </Card>
-            </Col>
-        </Container>
-        </div>
-    )
+				{props.data.map((value, id) => {
+					return (
+						<Col>
+							<Card className="card">
+								<Card.Text>
+									<span>
+										Monday, 20 July '20' - {value.schedule.times.berangkat}
+									</span>
+								</Card.Text>
+								<Card.Text className="text-row1">
+									<span>
+										<b>
+											{value.schedule.tujuanAwal.kota}
+											<img
+												src="https://res.cloudinary.com/rizkazn/image/upload/v1629481935/Vector_opjk1b.jpg"
+												style={{
+													paddingLeft: '20px',
+													paddingRight: '20px',
+												}}
+												alt=""
+											/>
+											{value.schedule.tujuanAkhir.kota}
+										</b>
+									</span>
+								</Card.Text>
+								<Card.Text className="">
+									<span>
+										{value.schedule.Maskapai.nameMaskapai},{' '}
+										{value.schedule.code}{' '}
+									</span>
+								</Card.Text>
+								<hr />
+								<br />
+								<Row>
+									<Col sm={3}>
+										<Card.Text>
+											<span>
+												<b>Status</b>
+											</span>
+										</Card.Text>
+									</Col>
+									<Col sm={5}>
+										<span className="status">Waiting for payment</span>
+									</Col>
+									<Col sm={4}>
+										<Dropdown>
+											<Dropdown.Toggle
+												style={{
+													float: 'right',
+													borderStyle: 'none',
+													outline: 'none',
+													background: 'none',
+													color: '#2395FF',
+												}}
+											>
+												View Details
+											</Dropdown.Toggle>
+										</Dropdown>
+									</Col>
+								</Row>
+							</Card>
+						</Col>
+					);
+				})}
+			</Container>
+		</div>
+	);
+};
 
-}
-
-export default mbCard
+export default MbCard;
